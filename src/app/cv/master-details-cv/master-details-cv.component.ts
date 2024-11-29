cceb8c8d51226c997aec6ca70f30020d3e12e92f
Ala Eddine Achach <122971085+Ala-Eddine-Achach@users.noreply.github.com>
2024-11-29 15:41:39 +0100

    add products

---
+++ src/app/cv/master-details-cv/master-details-cv.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-master-details-cv',
+  standalone: true,
+  imports: [],
+  templateUrl: './master-details-cv.component.html',
+  styleUrl: './master-details-cv.component.css'
+})
+export class MasterDetailsCvComponent {
+
+}
